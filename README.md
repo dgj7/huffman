75b31d28ca9429d7fb34f9e7721a2832f76a3b04
dg <garciad48879@gmail.com>
2024-02-10 18:05:12 -0500

    root: create a better script for building and running all the applications

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ huffman coding trees, implemented in various languages.
 generally treated as a programming challenge, to be redone for any new language learned.
 
 ## Usage
-There is a `run-all.sh` script in the root directory that can be used to run all projects together, to compare outputs.
+There is a `build-and-run-all.sh` script in the root directory that can be used to run all projects together, to compare outputs.
 
-Each directory has its own `build.sh` and `run.sh` scripts to build and run the individual application.
+Each directory has its own `build.sh` and `run.sh` scripts to build and run the individual applications.
 
 ## Design
 Each huffman implementation should accept input from the terminal with the `-i` flag, and generate the following output:
---
+++ build-and-run-all.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# build c++-huffman
+printf "building c++-huffman..."
+(cd c++-huffman ; ./build.sh > /dev/null 2>&1)
+if [ $? -eq 0 ]; then
+    printf " done.\n"
+else
+    printf " failed!\n"
+    exit 1
+fi
+
+# build java-huffman
+printf "building java-huffman..."
+(cd java-huffman ; ./build.sh > /dev/null 2>&1)
+if [ $? -eq 0 ]; then
+    printf " done.\n"
+else
+    printf " failed!\n"
+    exit 1
+fi
+
+# build rusthuffman
+printf "building rust-huffman..."
+(cd rust-huffman ; ./build.sh > /dev/null 2>&1)
+if [ $? -eq 0 ]; then
+    printf " done.\n"
+else
+    printf " failed!\n"
+    exit 1
+fi
+
+# if we get this far, all builds succeeded and we're ready to run the apps
+printf " ------------------------------|||c++ |||------------------------------\n"
+(cd c++-huffman ; ./run.sh)
+printf " ------------------------------|||java|||------------------------------\n"
+(cd java-huffman ; ./run.sh)
+printf " ------------------------------|||rust|||------------------------------\n"
+(cd rust-huffman ; ./run.sh)
+printf " ----------------------------------------------------------------------\n"
