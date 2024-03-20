22202b5f3117aef2dc84ed1d76826276ca11505e
dg <garciad48879@gmail.com>
2024-03-19 21:07:32 -0400

    go: first attempt at decoding; it's incorrect, but this is a start

--- go-huffman/htree/decoder.go
+++ go-huffman/htree/decoder.go
@@ -1,5 +1,25 @@
 package htree
 
 func Decode(tree HuffTree, encoded BitSet) string {
-	return "todo"
+	var result = ""
+	var node = tree.Root;
+
+	var i uint64 = 0
+
+	for i < encoded.Len() {
+		var bit = encoded.GetBit(i)
+
+		if !bit && node.Right != nil {
+			node = node.Right
+		} else if bit && node.Left != nil {
+			node = node.Left
+		} else {
+			result = result + string(node.Symbol)
+			node = tree.Root
+		}
+
+		i = i + 1
+	}
+
+	return result
 }
