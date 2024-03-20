4551345ef708fddddc91b8a5a2d591fce4bfefbf
dg <garciad48879@gmail.com>
2024-03-19 21:09:42 -0400

    go: move ToEncodingTable to htree file

--- go-huffman/htree/encoder.go
+++ go-huffman/htree/encoder.go
@@ -2,7 +2,7 @@ package htree
 
 func Encode(tree HuffTree, input string) BitSet {
 	var output = NewBitSet(0)
-	var table = toEncodingTable(tree)
+	var table = tree.ToEncodingTable()
 
 	for _, char := range input {
 		if bs, ok := table[char]; ok {
@@ -13,37 +13,4 @@ func Encode(tree HuffTree, input string) BitSet {
 	}
 
 	return output
-}
-
-func toEncodingTable(tree HuffTree) map[rune]BitSet {
-	var output = make(map[rune]BitSet)
-	var capacity = uint64(MaxDepth(tree.Root))
-	descendTree(tree, output, capacity)
-	return output
-}
-
-func descendTree(tree HuffTree, table map[rune]BitSet, capacity uint64) {
-	if tree.Root.Left != nil {
-		descendNode(tree.Root.Left, capacity, 0, false, make([]bool, capacity), table)
-	}
-	if tree.Root.Right != nil {
-		descendNode(tree.Root.Right, capacity, 0, true, make([]bool, capacity), table)
-	}
-}
-
-func descendNode(node *HuffNode, capacity uint64, index uint64, next bool, bs []bool, table map[rune]BitSet) {
-	bs[index] = next
-	if (node.Left != nil && node.Right != nil) {
-		descendNode(node.Left, capacity, index + 1, false, bs, table)
-		descendNode(node.Right, capacity, index + 1, true, bs, table)
-	} else {
-		bitset := NewBitSet(index+1)
-		var i uint64 = 0
-		for i < index+1 {
-			var b = bs[i]
-			bitset.SetBit(i, b)
-			i = i + 1
-		}
-		table[node.Symbol] = bitset
-	}
 }
--- go-huffman/htree/htree.go
+++ go-huffman/htree/htree.go
@@ -131,3 +131,36 @@ func nodeSize(root *HuffNode) uint64 {
 func isLeaf(node *HuffNode) bool {
 	return node.Left == nil && node.Right == nil
 }
+
+func (tree HuffTree) ToEncodingTable() map[rune]BitSet {
+	var output = make(map[rune]BitSet)
+	var capacity = uint64(MaxDepth(tree.Root))
+	descendTree(tree, output, capacity)
+	return output
+}
+
+func descendTree(tree HuffTree, table map[rune]BitSet, capacity uint64) {
+	if tree.Root.Left != nil {
+		descendNode(tree.Root.Left, capacity, 0, false, make([]bool, capacity), table)
+	}
+	if tree.Root.Right != nil {
+		descendNode(tree.Root.Right, capacity, 0, true, make([]bool, capacity), table)
+	}
+}
+
+func descendNode(node *HuffNode, capacity uint64, index uint64, next bool, bs []bool, table map[rune]BitSet) {
+	bs[index] = next
+	if (node.Left != nil && node.Right != nil) {
+		descendNode(node.Left, capacity, index + 1, false, bs, table)
+		descendNode(node.Right, capacity, index + 1, true, bs, table)
+	} else {
+		bitset := NewBitSet(index+1)
+		var i uint64 = 0
+		for i < index+1 {
+			var b = bs[i]
+			bitset.SetBit(i, b)
+			i = i + 1
+		}
+		table[node.Symbol] = bitset
+	}
+}
