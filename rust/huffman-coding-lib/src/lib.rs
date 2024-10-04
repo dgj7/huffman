0b33d24095796a61a25c39c6edba43eb1ffc7370
dgj7 <garciad48879@gmail.com>
2024-10-03 22:32:02 -0400

    rust: start splitting into a crate, and a program using the crate

---
+++ rust/huffman-coding-lib/src/lib.rs
@@ -0,0 +1,50 @@
+
+use std::collections::HashMap;
+use core::str::Chars;
+use bitvec::vec::BitVec;
+
+pub const LEFT: bool = false;
+pub const RIGHT: bool = true;
+
+pub trait Input {
+    fn to_vector(&self) -> Vec<TreeNode>;
+    fn to_char_array(&self) -> Chars;
+}
+
+pub struct StringInput {
+    pub message: String,
+}
+
+#[derive(Clone)]
+pub struct TreeNode {
+    pub symbol: Option<char>,
+    pub frequency: usize,
+    pub left: Option<Box<TreeNode>>,
+    pub right: Option<Box<TreeNode>>,
+}
+
+pub struct HuffmanTree {
+    pub root: TreeNode,
+    pub encodings: HashMap<char, BitVec>,
+}
+
+pub struct Encoded {
+    pub bits: BitVec,
+}
+
+pub struct DecodedByte {
+    pub symbol: char,
+    pub bits: BitVec,
+}
+
+pub struct DecodedString {
+    pub message: String,
+}
+
+pub struct PrintableBitVec {
+    pub bits: BitVec,
+}
+
+
+
+
---
+++ rust/test-program/src/main.rs
@@ -0,0 +1,8 @@
+extern crate huffman_coding_lib;
+
+fn main() {
+    let start = std::time::Instant::now();
+
+
+    println!("done. ({}ms)", start.elapsed().as_millis());
+}
