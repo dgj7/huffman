ae94633a9a98d721e67079de90add054bcc22458
dgj7 <garciad48879@gmail.com>
2025-08-08 17:57:06 -0400

    rust: migrate example program stuff into main file to make migration into examples directory easier

--- rust/test-program/src/main.rs
+++ rust/test-program/src/main.rs
@@ -1,12 +1,10 @@
-mod translator;
-mod printable;
-
+use std::fmt::{Display, Formatter};
+use bitvec::macros::internal::funty::Fundamental;
+use bitvec::prelude::BitVec;
 use huffman_coding_lib::decode;
 use huffman_coding_lib::encode;
 use huffman_coding_lib::frequencies_to_tree;
 use huffman_coding_lib::count_frequencies;
-use crate::printable::PrintableBitVec;
-use crate::translator::{message_to_bytes, bytes_to_message};
 
 extern crate huffman_coding_lib;
 
@@ -14,7 +12,7 @@ fn main() {
     let start = std::time::Instant::now();
     let input = parse(std::env::args());
 
-    let bytes = message_to_bytes(&input);
+    let bytes = string_to_bytes(&input);
     let frequencies = count_frequencies(&bytes);
     let tree = frequencies_to_tree(&frequencies);
     let mut encoded = encode(&frequencies, &tree);
@@ -36,18 +34,64 @@ fn parse(args: std::env::Args) -> String {
     }
 }
 
+///
+/// Convert input String into byte [Vector].
+///
+pub fn string_to_bytes(message : &String) -> Vec<u8> {
+    let mut vec = vec!();
+
+    for c in message.chars() {
+        vec.push(c as u8);
+    }
+
+    return vec;
+}
+
+///
+/// Translate output byte [Vector] into String.
+///
+pub fn bytes_to_message(bytes : &Vec<u8>) -> String {
+    let mut sb = String::new();
+
+    for b in bytes {
+        let c = *b as char;
+        sb.push(c);
+    }
+
+    return sb;
+}
+
+struct PrintableBitVec {
+    bits: BitVec,
+}
+
+impl PrintableBitVec {
+    pub(crate) fn new(the_bits: &BitVec) -> PrintableBitVec {
+        PrintableBitVec { bits: the_bits.clone() }
+    }
+}
+
+impl Display for PrintableBitVec {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        let mut value = String::new();
+        self.bits.iter().for_each(|bit| {
+            value.push_str(bit.as_u8().to_string().as_str());
+        });
+        write!(f, "{}", value)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use huffman_coding_lib::{count_frequencies, decode, encode, frequencies_to_tree};
-    use crate::printable::PrintableBitVec;
-    use crate::translator::{bytes_to_message, message_to_bytes};
+    use crate::{bytes_to_message, string_to_bytes, PrintableBitVec};
 
     #[test]
     fn test_main_scenario() {
         let mut input = String::new();
         input.push_str("this is a sample input string. its text is being used to test the huffman coding tree.");
 
-        let bytes = message_to_bytes(&input);
+        let bytes = string_to_bytes(&input);
         let frequencies = count_frequencies(&bytes);
         let tree = frequencies_to_tree(&frequencies);
 
@@ -58,5 +102,33 @@ mod test {
         let decoded = decode(&mut encoded, tree);
         let translated = bytes_to_message(&decoded);
         assert_eq!("this is a sample input string. its text is being used to test the huffman coding tree.", translated);
+    }
+
+    #[test]
+    fn test_string_to_bytes() {
+        let message = "Hello World!".to_string();
+        let bytes = string_to_bytes(&message);
+
+        assert_eq!(12, bytes.len());
+        assert_eq!(72, bytes[0]);
+        assert_eq!(101, bytes[1]);
+        assert_eq!(108, bytes[2]);
+        assert_eq!(108, bytes[3]);
+        assert_eq!(111, bytes[4]);
+        assert_eq!(32, bytes[5]);
+        assert_eq!(87, bytes[6]);
+        assert_eq!(111, bytes[7]);
+        assert_eq!(114, bytes[8]);
+        assert_eq!(108, bytes[9]);
+        assert_eq!(100, bytes[10]);
+        assert_eq!(33, bytes[11]);
+    }
+
+    #[test]
+    fn test_bytes_to_string() {
+        let bytes = vec![72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33];
+        let message = bytes_to_message(&bytes);
+
+        assert_eq!(String::from("Hello World!"), message);
     }
 }
