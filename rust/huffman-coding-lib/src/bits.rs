ea0e40e774f2d698cc00495d18e17bf8977a63a2
dgj7 <garciad48879@gmail.com>
2025-08-08 16:05:26 -0400

    rust: bits: implement extract()

--- rust/huffman-coding-lib/src/bits.rs
+++ rust/huffman-coding-lib/src/bits.rs
@@ -8,8 +8,8 @@ use bitvec::macros::internal::funty::Fundamental;
 pub struct Bits {
     storage: Vec<u8>,
     #[allow(dead_code)]// todo
-    bit_capacity: u64,
-    bit_length: u64,
+    bit_capacity: usize,
+    bit_length: usize,
 }
 
 ///
@@ -17,7 +17,7 @@ pub struct Bits {
 ///
 pub struct BitsIterator<'a> {
     bits: &'a Bits,
-    bit_index: u64,
+    bit_index: usize,
 }
 
 ///
@@ -25,7 +25,7 @@ pub struct BitsIterator<'a> {
 ///
 pub struct BytesIterator<'a> {
     bits: &'a Bits,
-    byte_index: u64,
+    byte_index: usize,
 }
 
 impl Bits {
@@ -47,8 +47,8 @@ impl Bits {
         }
 
         /* determine bit and byte indices within the storage, based on the current length */
-        let bit_index: usize = (self.bit_length % 8) as usize;
-        let byte_index: usize = (self.bit_length / 8) as usize;
+        let bit_index: usize = self.bit_length % 8;
+        let byte_index: usize = self.bit_length / 8;
 
         /* increment length for the next call */
         self.bit_length += 1;
@@ -57,7 +57,7 @@ impl Bits {
         let byte = &mut self.storage[byte_index];
 
         /* write the bit to the byte */
-        write_to_byte(byte, bit_index as u8, bit);
+        write_to_byte(byte, bit_index, bit);
     }
 
     pub fn append(&mut self, other: &Bits) {
@@ -79,9 +79,65 @@ impl Bits {
             byte_index: 0,
         }
     }
+
+    ///
+    /// Extract (start, start+count-1) bits and return as a new [Bits].
+    ///
+    pub fn extract(&mut self, start: usize, count: usize) -> Bits {
+        /* storage for output */
+        let mut bits = Bits::new();
+
+        /* iterate over the bits specified by the range */
+        for current_index in start..start + count {
+            /* determine the value of the replacement bit; it either comes from later in the array, or it's 0 because there isn't a later */
+            let replacement_index = start + count;
+            let replacement_byte_index = replacement_index / 8;
+            let replacement_bit: bool = if replacement_byte_index > self.storage.len() - 1 {
+                false
+            } else {
+                let replacement_source_byte = self.storage[replacement_byte_index];
+                let replacement_bit_index = replacement_index % 8;
+                read_from_byte(replacement_source_byte, replacement_bit_index)
+            };
+
+            /* get the value of the current bit, and store it in the output */
+            let current_byte_index = current_index / 8;
+            let mut current_byte = self.storage[current_byte_index];
+            let current_bit_index = current_index % 8;
+            let current_bit = read_from_byte(current_byte, current_bit_index);
+            bits.push(current_bit);
+
+            /* overwrite the current bit index with the replacement bit */
+            write_to_byte(&mut current_byte, current_bit_index, replacement_bit);
+        }
+
+        /* remove (zero) the last 'count' bits */
+        for remove_index in (self.bit_capacity - 1 - count) .. self.bit_capacity {
+            let remove_byte_index = remove_index / 8;
+            let remove_bit_index = remove_index % 8;
+            let remove_byte = &mut self.storage[remove_byte_index];
+            write_to_byte(remove_byte, remove_bit_index, false);
+        }
+
+        /* update size */
+        self.bit_length -= count;
+
+        /* determine if any storage bytes can be freed */
+        let needed_storage_bytes = self.bit_capacity / 8;
+        let actual_storage_bytes = self.storage.len();
+        if needed_storage_bytes < actual_storage_bytes {
+            self.storage.truncate(needed_storage_bytes);
+        }
+
+        /* update capacity; must always be multiple of 8 */
+        self.bit_capacity = self.storage.len() * 8;
+
+        /* done */
+        bits
+    }
 }
 
-fn write_to_byte(byte: &mut u8, bit_index: u8, bit: bool) {
+fn write_to_byte(byte: &mut u8, bit_index: usize, bit: bool) {
     if bit_index >= 8 {
         panic!("Index out of bounds");
     }
@@ -93,7 +149,7 @@ fn write_to_byte(byte: &mut u8, bit_index: u8, bit: bool) {
     *byte |= flag;
 }
 
-fn read_from_byte(byte: u8, index: u8) -> bool {
+fn read_from_byte(byte: u8, index: usize) -> bool {
     if index >= 8 {
         panic!("Index out of bounds");
     }
@@ -106,13 +162,13 @@ impl<'a> Iterator for BitsIterator<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.bit_index < self.bits.bit_length {
-            let bit_index = self.bit_index % 8;
-            let byte_index = self.bit_index / 8;
+            let bit_index: usize = (self.bit_index % 8) as usize;
+            let byte_index: usize = (self.bit_index / 8) as usize;
 
             self.bit_index += 1;
 
             let byte = self.bits.storage[byte_index as usize];
-            let result = read_from_byte(byte, bit_index as u8);
+            let result = read_from_byte(byte, bit_index);
 
             Some(result)
         } else {
@@ -153,12 +209,18 @@ mod test {
     fn test_alternate_true_first() {
         let mut bits = Bits::new();
 
+        /* test push */
         bits.push(true);
         bits.push(false);
-        bits.push(true);
-        bits.push(false);
+        assert_eq!("10", bits.to_string());
+        assert_eq!(2, bits.bit_length);
+        assert_eq!(8, bits.bit_capacity);
 
-        /* test storage/push, read/write bits, and bits iterator */
+        /* test append */
+        let mut appendage = Bits::new();
+        appendage.push(true);
+        appendage.push(false);
+        bits.append(&appendage);
         assert_eq!("1010", bits.to_string());
         assert_eq!(4, bits.bit_length);
         assert_eq!(8, bits.bit_capacity);
@@ -179,18 +241,36 @@ mod test {
         assert_eq!("1010", clone.to_string());
         assert_eq!(4, clone.bit_length);
         assert_eq!(8, clone.bit_capacity);
+
+        /* test extract */
+        let extracted = bits.extract(1, 2);
+        assert_eq!("01", extracted.to_string());
+        assert_eq!(2, extracted.bit_length);
+        assert_eq!(8, extracted.bit_capacity);
+        assert_eq!("10", bits.to_string());
+        assert_eq!(2, bits.bit_length);
+        assert_eq!(8, bits.bit_capacity);
+        assert_eq!("1010", clone.to_string());
+        assert_eq!(4, clone.bit_length);
+        assert_eq!(8, clone.bit_capacity);
     }
 
     #[test]
     fn test_alternate_false_first() {
         let mut bits = Bits::new();
 
-        bits.push(false);//0
-        bits.push(true);//1
+        /* test push */
         bits.push(false);
         bits.push(true);
+        assert_eq!("01", bits.to_string());
+        assert_eq!(2, bits.bit_length);
+        assert_eq!(8, bits.bit_capacity);
 
-        /* test storage/push, read/write bits, and bits iterator */
+        /* test append */
+        let mut appendage = Bits::new();
+        appendage.push(false);
+        appendage.push(true);
+        bits.append(&appendage);
         assert_eq!("0101", bits.to_string());
         assert_eq!(4, bits.bit_length);
         assert_eq!(8, bits.bit_capacity);
@@ -211,18 +291,36 @@ mod test {
         assert_eq!("0101", clone.to_string());
         assert_eq!(4, clone.bit_length);
         assert_eq!(8, clone.bit_capacity);
+
+        /* test extract */
+        let extracted = bits.extract(1, 2);
+        assert_eq!("10", extracted.to_string());
+        assert_eq!(2, extracted.bit_length);
+        assert_eq!(8, extracted.bit_capacity);
+        assert_eq!("01", bits.to_string());
+        assert_eq!(2, bits.bit_length);
+        assert_eq!(8, bits.bit_capacity);
+        assert_eq!("0101", clone.to_string());
+        assert_eq!(4, clone.bit_length);
+        assert_eq!(8, clone.bit_capacity);
     }
 
     #[test]
     fn test_only_true() {
         let mut bits = Bits::new();
 
+        /* test push */
         bits.push(true);
         bits.push(true);
-        bits.push(true);
-        bits.push(true);
+        assert_eq!("11", bits.to_string());
+        assert_eq!(2, bits.bit_length);
+        assert_eq!(8, bits.bit_capacity);
 
-        /* test storage/push, read/write bits, and bits iterator */
+        /* test append */
+        let mut appendage = Bits::new();
+        appendage.push(true);
+        appendage.push(true);
+        bits.append(&appendage);
         assert_eq!("1111", bits.to_string());
         assert_eq!(4, bits.bit_length);
         assert_eq!(8, bits.bit_capacity);
@@ -243,18 +341,36 @@ mod test {
         assert_eq!("1111", clone.to_string());
         assert_eq!(4, clone.bit_length);
         assert_eq!(8, clone.bit_capacity);
+
+        /* test extract */
+        let extracted = bits.extract(1, 2);
+        assert_eq!("11", extracted.to_string());
+        assert_eq!(2, extracted.bit_length);
+        assert_eq!(8, extracted.bit_capacity);
+        assert_eq!("11", bits.to_string());
+        assert_eq!(2, bits.bit_length);
+        assert_eq!(8, bits.bit_capacity);
+        assert_eq!("1111", clone.to_string());
+        assert_eq!(4, clone.bit_length);
+        assert_eq!(8, clone.bit_capacity);
     }
 
     #[test]
     fn test_only_false() {
         let mut bits = Bits::new();
 
+        /* test push */
         bits.push(false);
         bits.push(false);
-        bits.push(false);
-        bits.push(false);
+        assert_eq!("00", bits.to_string());
+        assert_eq!(2, bits.bit_length);
+        assert_eq!(8, bits.bit_capacity);
 
-        /* test storage/push, read/write bits, and bits iterator */
+        /* test append */
+        let mut appendage = Bits::new();
+        appendage.push(false);
+        appendage.push(false);
+        bits.append(&appendage);
         assert_eq!("0000", bits.to_string());
         assert_eq!(4, bits.bit_length);
         assert_eq!(8, bits.bit_capacity);
@@ -275,17 +391,38 @@ mod test {
         assert_eq!("0000", clone.to_string());
         assert_eq!(4, clone.bit_length);
         assert_eq!(8, clone.bit_capacity);
+
+        /* test extract */
+        let extracted = bits.extract(1, 2);
+        assert_eq!("00", extracted.to_string());
+        assert_eq!(2, extracted.bit_length);
+        assert_eq!(8, extracted.bit_capacity);
+        assert_eq!("00", bits.to_string());
+        assert_eq!(2, bits.bit_length);
+        assert_eq!(8, bits.bit_capacity);
+        assert_eq!("0000", clone.to_string());
+        assert_eq!(4, clone.bit_length);
+        assert_eq!(8, clone.bit_capacity);
     }
 
     #[test]
     fn test_carry_over_true() {
         let mut bits = Bits::new();
 
-        for _ in 0..35 {
+        /* test push */
+        for _ in 0..28 {
             bits.push(true);
         }// 4 bytes, +3 bits
+        assert_eq!("1111111111111111111111111111", bits.to_string());
+        assert_eq!(28, bits.bit_length);
+        assert_eq!(32, bits.bit_capacity);
 
-        /* test storage/push, read/write bits, and bits iterator */
+        /* test append */
+        let mut appendage = Bits::new();
+        for _ in 0 .. 7 {
+            appendage.push(true);
+        }
+        bits.append(&appendage);
         assert_eq!("11111111111111111111111111111111111", bits.to_string());
         assert_eq!(35, bits.bit_length);
         assert_eq!(40, bits.bit_capacity);
@@ -318,17 +455,38 @@ mod test {
         assert_eq!("11111111111111111111111111111111111", clone.to_string());
         assert_eq!(35, clone.bit_length);
         assert_eq!(40, clone.bit_capacity);
+
+        /* test extract */
+        let extracted = bits.extract(1, 2);
+        assert_eq!("11", extracted.to_string());
+        assert_eq!(2, extracted.bit_length);
+        assert_eq!(8, extracted.bit_capacity);
+        assert_eq!("111111111111111111111111111111111", bits.to_string());
+        assert_eq!(33, bits.bit_length);
+        assert_eq!(40, bits.bit_capacity);
+        assert_eq!("11111111111111111111111111111111111", clone.to_string());
+        assert_eq!(35, clone.bit_length);
+        assert_eq!(40, clone.bit_capacity);
     }
 
     #[test]
     fn test_carry_over_false() {
         let mut bits = Bits::new();
 
-        for _ in 0..35 {
+        /* test push */
+        for _ in 0..26 {
             bits.push(false);
         }// 4 bytes, +3 bits
+        assert_eq!("00000000000000000000000000", bits.to_string());
+        assert_eq!(26, bits.bit_length);
+        assert_eq!(32, bits.bit_capacity);
 
-        /* test storage/push, read/write bits, and bits iterator */
+        /* test append */
+        let mut appendage = Bits::new();
+        for _ in 0 .. 9 {
+            appendage.push(false);
+        }
+        bits.append(&appendage);
         assert_eq!("00000000000000000000000000000000000", bits.to_string());
         assert_eq!(35, bits.bit_length);
         assert_eq!(40, bits.bit_capacity);
@@ -361,43 +519,135 @@ mod test {
         assert_eq!("00000000000000000000000000000000000", clone.to_string());
         assert_eq!(35, clone.bit_length);
         assert_eq!(40, clone.bit_capacity);
+
+        /* test extract */
+        let extracted = bits.extract(1, 2);
+        assert_eq!("00", extracted.to_string());
+        assert_eq!(2, extracted.bit_length);
+        assert_eq!(8, extracted.bit_capacity);
+        assert_eq!("000000000000000000000000000000000", bits.to_string());
+        assert_eq!(33, bits.bit_length);
+        assert_eq!(40, bits.bit_capacity);
+        assert_eq!("00000000000000000000000000000000000", clone.to_string());
+        assert_eq!(35, clone.bit_length);
+        assert_eq!(40, clone.bit_capacity);
     }
 
     #[test]
-    fn test_append() {
-        let mut left = Bits::new();
-        left.push(true);
-        left.push(false);
-        left.push(true);
-        left.push(false);
-        left.push(true);
-        left.push(false);
-        left.push(true);
-        left.push(false);
-        left.push(true);
-        left.push(false);
-        left.push(true);
-        let mut right = Bits::new();
-        right.push(true);
-        right.push(false);
-        right.push(true);
-        right.push(false);
-        right.push(true);
-        right.push(false);
-        right.push(true);
-        right.push(false);
-        right.push(true);
+    fn test_scenario1() {
+        let mut bits = Bits::new();
 
-        left.append(&right);
+        /* test push */
+        bits.push(true);
+        bits.push(false);
+        bits.push(true);
+        bits.push(false);
+        bits.push(true);
+        bits.push(false);
+        bits.push(true);
+        bits.push(false);
+        bits.push(true);
+        bits.push(false);
+        bits.push(true);
+        assert_eq!("10101010101", bits.to_string());
+        assert_eq!(11, bits.bit_length);
+        assert_eq!(16, bits.bit_capacity);
 
-        assert_eq!("10101010101101010101", left.to_string());
-        assert_eq!(20, left.to_string().len());
-        assert_eq!(20, left.bit_length);
-        assert_eq!(24, left.bit_capacity);
+        /* test append */
+        let mut appendage = Bits::new();
+        appendage.push(true);
+        appendage.push(false);
+        appendage.push(true);
+        appendage.push(false);
+        appendage.push(true);
+        appendage.push(false);
+        appendage.push(true);
+        appendage.push(false);
+        appendage.push(true);
+        bits.append(&appendage);
+        assert_eq!("10101010101101010101", bits.to_string());
+        assert_eq!(20, bits.bit_length);
+        assert_eq!(24, bits.bit_capacity);
 
-        assert_eq!("101010101", right.to_string());
-        assert_eq!(9, right.to_string().len());
-        assert_eq!(9, right.bit_length);
-        assert_eq!(16, right.bit_capacity);
+        /* test bytes iterator */
+        let mut byte_iterator = bits.bytes_iter();
+        let byte1 = byte_iterator.next().unwrap();
+        let byte2 = byte_iterator.next().unwrap();
+        let byte3 = byte_iterator.next().unwrap();
+        let byte4 = byte_iterator.next();
+        assert_eq!(85, byte1);
+        assert_eq!(173, byte2);
+        assert_eq!(10, byte3);
+        assert_eq!(None, byte4);
+
+        /* test clone */
+        let clone = bits.clone();
+        assert_eq!("10101010101101010101", clone.to_string());
+        assert_eq!(20, clone.bit_length);
+        assert_eq!(24, clone.bit_capacity);
+
+        /* test extract */
+        let extracted = bits.extract(1, 2);
+        assert_eq!("01", extracted.to_string());
+        assert_eq!(2, extracted.bit_length);
+        assert_eq!(8, extracted.bit_capacity);
+        assert_eq!("101010101011010101", bits.to_string());
+        assert_eq!(18, bits.bit_length);
+        assert_eq!(24, bits.bit_capacity);
+        assert_eq!("10101010101101010101", clone.to_string());
+        assert_eq!(20, clone.bit_length);
+        assert_eq!(24, clone.bit_capacity);
+    }
+
+    #[test]
+    fn test_scenario2() {
+        let mut bits = Bits::new();
+
+        /* test push */
+        for _ in 0 .. 16 {
+            bits.push(true);
+        }
+        assert_eq!("1111111111111111", bits.to_string());
+        assert_eq!(16, bits.bit_length);
+        assert_eq!(16, bits.bit_capacity);
+
+        /* test append */
+        let mut appendage = Bits::new();
+        for _ in 0 .. 3 {
+            appendage.push(true);
+        }
+        bits.append(&appendage);
+        assert_eq!("1111111111111111111", bits.to_string());
+        assert_eq!(19, bits.bit_length);
+        assert_eq!(24, bits.bit_capacity);
+
+        /* test bytes iterator */
+        let mut byte_iterator = bits.bytes_iter();
+        let byte1 = byte_iterator.next().unwrap();
+        let byte2 = byte_iterator.next().unwrap();
+        let byte3 = byte_iterator.next().unwrap();
+        let byte4 = byte_iterator.next();
+        assert_eq!(255, byte1);
+        assert_eq!(255, byte2);
+        assert_eq!(7, byte3);
+        assert_eq!(None, byte4);
+
+        /* test clone */
+        let clone = bits.clone();
+        assert_eq!("1111111111111111111", clone.to_string());
+        assert_eq!(19, clone.bit_length);
+        assert_eq!(24, clone.bit_capacity);
+
+        /* test extract */
+        let extracted = bits.extract(10, 3);
+        assert_eq!("111", extracted.to_string());
+        assert_eq!(3, extracted.bit_length);
+        assert_eq!(8, extracted.bit_capacity);
+        assert_eq!("1111111111111111", bits.to_string());
+        assert_eq!(16, bits.bit_length);
+        assert_eq!(24, bits.bit_capacity);
+        assert_eq!("1111111111111111111", clone.to_string());
+        assert_eq!(19, clone.bit_length);
+        assert_eq!(24, clone.bit_capacity);
     }
 }
