b0ed2e09afc180c93b73fde4c2305184b1bdb802
dgj7 <garciad48879@gmail.com>
2025-10-18 23:43:44 -0400

    root: refactor build/run script to build-all

--- build-all.sh
+++ build-all.sh
@@ -2,7 +2,7 @@
 
 # build c++
 printf "building for c++..."
-(cd c++ ; ./build.sh > /dev/null 2>&1)
+(cd cpp ; ./build.sh > /dev/null 2>&1)
 if [ $? -eq 0 ]; then
     printf " done.\n"
 else
@@ -49,16 +49,3 @@ else
     printf " failed!\n"
     exit 1
 fi
-
-# if we get this far, all builds succeeded and we're ready to run the apps
-printf " ------------------------------|||c++ |||------------------------------\n"
-(cd c++ ; ./run.sh)
-printf " ------------------------------|||rust|||------------------------------\n"
-(cd rust ; ./run.sh)
-printf " ------------------------------||||go||||------------------------------\n"
-(cd go ; ./run.sh)
-printf " ------------------------------|||java|||------------------------------\n"
-(cd java ; ./run.sh)
-printf " ------------------------------||||c|||||------------------------------\n"
-(cd c ; ./run.sh)
-printf " ----------------------------------------------------------------------\n"
