590023e5116c6dcce6a1c71ea216ad6889de8b71
dgj7 <garciad48879@gmail.com>
2025-10-19 00:16:53 -0400

    testall: refactor build script to be simpler

--- build-all.sh
+++ build-all.sh
@@ -1,51 +1,18 @@
 #!/bin/bash
 
-# build c
-printf "building for c..."
-(cd c ; ./build.sh > /dev/null 2>&1)
-if [ $? -eq 0 ]; then
+function do_build() {
+    printf "building for $1..."
+    (cd $1; ./build.sh > /dev/null 2>&1)
+    if [ $? -eq 0 ]; then
     printf " done.\n"
-else
-    printf " failed!\n"
-    exit 1
-fi
+    else
+        printf " failed!\n"
+        exit 1
+    fi
+}
 
-# build c++
-printf "building for c++..."
-(cd cpp ; ./build.sh > /dev/null 2>&1)
-if [ $? -eq 0 ]; then
-    printf " done.\n"
-else
-    printf " failed!\n"
-    exit 1
-fi
-
-# build rust
-printf "building for rust..."
-(cd rust ; ./build.sh > /dev/null 2>&1)
-if [ $? -eq 0 ]; then
-    printf " done.\n"
-else
-    printf " failed!\n"
-    exit 1
-fi
-
-# build go
-printf "building for go..."
-(cd go ; ./build.sh > /dev/null 2>&1)
-if [ $? -eq 0 ]; then
-    printf " done.\n"
-else
-    printf " failed!\n"
-    exit 1
-fi
-
-# build java
-printf "building for java..."
-(cd java ; ./build.sh > /dev/null 2>&1)
-if [ $? -eq 0 ]; then
-    printf " done.\n"
-else
-    printf " failed!\n"
-    exit 1
-fi
+do_build "c"
+do_build "cpp"
+do_build "rust"
+do_build "go"
+do_build "java"
