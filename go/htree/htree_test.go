f532ad5466989541b5d71e743048c63cfade0fe0
dgj7 <garciad48879@gmail.com>
2025-10-22 21:54:48 -0400

    go: more tests

--- go/htree/htree_test.go
+++ go/htree/htree_test.go
@@ -4,10 +4,21 @@ import "testing"
 import "github.com/stretchr/testify/assert" // https://pkg.go.dev/github.com/stretchr/testify/assert
 
 func TestToTreeEmpty(t *testing.T) {
-	var tree = Create("")
+	var input = ""
+
+	var tree = Create(input)
 
 	assert.Equal(t, uint64(0), tree.size(), "tree size wrong")
 	assert.Nil(t, tree.Root, "tree root not nil")
+
+	var encodings = tree.ToEncodingTable()
+	assert.Equal(t, 0, len(encodings), "encodings len wrong")
+
+	var encoded = Encode(tree, input)
+	assert.Equal(t, "", encoded.ToString(), "encoded wrong")
+
+	var decoded = Decode(tree, encoded)
+	assert.Equal(t, "", decoded, "decoded wrong")
 }
 
 func TestToTreeSmall(t *testing.T) {
@@ -24,12 +35,12 @@ func TestToTreeSmall(t *testing.T) {
 	assert.Equal(t, "0", encodings['c'].ToString(), "encoding wrong")
 
 	var encoded = Encode(tree, input)
+	assert.Equal(t, "101111000", encoded.ToString(), "encoded wrong")
 
 	var decoded = Decode(tree, encoded)
 	assert.Equal(t, "abbccc", decoded, "decoded wrong")
 }
 
-// todo: uncomment failing test asserts
 func TestToTreeScenario1(t *testing.T) {
 	var input = "this is a sample input string. its text is being used to test the huffman coding tree.";
 
@@ -42,22 +53,22 @@ func TestToTreeScenario1(t *testing.T) {
 	assert.Equal(t, "01010", encodings['o'].ToString(), "encoding wrong")
     assert.Equal(t, "01100", encodings['r'].ToString(), "encoding wrong")
     assert.Equal(t, "0111", encodings['n'].ToString(), "encoding wrong")
-    assert.Equal(t, "011011", encodings['c'].ToString(), "encoding wrong")// fails; this is correct! wants to be 101001, which is x
+    assert.Equal(t, "011011", encodings['c'].ToString(), "encoding wrong")
     assert.Equal(t, "111", encodings[' '].ToString(), "encoding wrong")
     assert.Equal(t, "01001", encodings['m'].ToString(), "encoding wrong")
-    assert.Equal(t, "101000", encodings['l'].ToString(), "encoding wrong")// fails; this is correct! wants to be 011010, which is b
+    assert.Equal(t, "101000", encodings['l'].ToString(), "encoding wrong")
     assert.Equal(t, "10101", encodings['a'].ToString(), "encoding wrong")
     assert.Equal(t, "11000", encodings['u'].ToString(), "encoding wrong")
     assert.Equal(t, "110011", encodings['d'].ToString(), "encoding wrong")
     assert.Equal(t, "000", encodings['i'].ToString(), "encoding wrong")
     assert.Equal(t, "100", encodings['t'].ToString(), "encoding wrong")
-    assert.Equal(t, "011010", encodings['b'].ToString(), "encoding wrong")// fails; this is correct! wants to be 101000, which is l
+    assert.Equal(t, "011010", encodings['b'].ToString(), "encoding wrong")
     assert.Equal(t, "10110", encodings['g'].ToString(), "encoding wrong")
     assert.Equal(t, "10111", encodings['h'].ToString(), "encoding wrong")
     assert.Equal(t, "110010", encodings['.'].ToString(), "encoding wrong")
     assert.Equal(t, "01000", encodings['f'].ToString(), "encoding wrong")
     assert.Equal(t, "01011", encodings['p'].ToString(), "encoding wrong")
-    assert.Equal(t, "101001", encodings['x'].ToString(), "encoding wrong")// fails; this is correct! wants to be 011011, which is c
+    assert.Equal(t, "101001", encodings['x'].ToString(), "encoding wrong")
     assert.Equal(t, "1101", encodings['e'].ToString(), "encoding wrong")
     assert.Equal(t, "001", encodings['s'].ToString(), "encoding wrong")
 
