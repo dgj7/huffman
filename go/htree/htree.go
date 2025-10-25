45ed906fb3562b24680b4e7924bae5e453474a70
dgj7 <garciad48879@gmail.com>
2025-10-25 18:53:59 -0400

    go: move function that's clearly in the wrong place

--- go/htree/htree.go
+++ go/htree/htree.go
@@ -43,3 +43,21 @@ func descendTree(tree HuffTree, table map[rune]BitSet) {
 		descendNode(tree.Root.Right, 0, true, make([]bool, tree.size()), table)
 	}
 }
+
+func descendNode(node *HuffNode, index int, next bool, bs []bool, table map[rune]BitSet) {
+	bs[index] = next
+	if (node.Left != nil && node.Right != nil) {
+		descendNode(node.Left, index + 1, false, bs, table)
+		descendNode(node.Right, index + 1, true, bs, table)
+	} else {
+		bitset := NewBitSet(index+1)
+		var i int = 0
+		for i < index+1 {
+			var b = bs[i]
+			bitset.SetBit(i, b)
+			i = i + 1
+		}
+		//fmt.Printf("%c ===> %s\n", node.Symbol, bitset.ToString())
+		table[node.Symbol] = bitset
+	}
+}
--- go/htree/node.go
+++ go/htree/node.go
@@ -25,21 +25,3 @@ func (node HuffNode) size() int {
 func (node HuffNode) isLeaf() bool {
 	return node.Left == nil && node.Right == nil
 }
-
-func descendNode(node *HuffNode, index int, next bool, bs []bool, table map[rune]BitSet) {
-	bs[index] = next
-	if (node.Left != nil && node.Right != nil) {
-		descendNode(node.Left, index + 1, false, bs, table)
-		descendNode(node.Right, index + 1, true, bs, table)
-	} else {
-		bitset := NewBitSet(index+1)
-		var i int = 0
-		for i < index+1 {
-			var b = bs[i]
-			bitset.SetBit(i, b)
-			i = i + 1
-		}
-		//fmt.Printf("%c ===> %s\n", node.Symbol, bitset.ToString())
-		table[node.Symbol] = bitset
-	}
-}
