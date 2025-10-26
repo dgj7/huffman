624fb7275ee7bbbac5b5629777888410029c497c
dgj7 <garciad48879@gmail.com>
2025-10-26 10:17:31 -0400

    go: rewrite GetBit so it's safer

--- go/htree/bitset.go
+++ go/htree/bitset.go
@@ -38,10 +38,20 @@ func (bs BitSet) Len() int {
 	return bs.bitLength
 }
 
-func (bs BitSet) GetBit(index int) bool {
-	var position = index / 8
-	var shift = uint(index % 8)
-	return (bs.data[position] & (byte(1) << shift)) != 0
+func (bs BitSet) GetBit(bitIndex int) bool {
+	/* determine where the new data needs to live */
+	var byteLength = len(bs.data)
+	var byteIndex = bitIndex / 8
+	var shift = uint(bitIndex % 8)
+
+	/* handle bad byte index */
+	if byteIndex >= byteLength {
+		var message = fmt.Sprintf("GetBit(%d): byteLength=[%d]\n", byteIndex, byteLength)
+		panic(message)
+	}
+
+	/* read and return the data */
+	return (bs.data[byteIndex] & (byte(1) << shift)) != 0
 }
 
 func (bs *BitSet) Append(value bool) {
