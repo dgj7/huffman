9145b96e91d1f05ad43a37731b84285a3b98efa3
dgj7 <garciad48879@gmail.com>
2025-10-28 19:56:33 -0400

    go: come up with a way to convert string to bytes

--- go/main.go
+++ go/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 import "flag"
 import "os"
+import "unicode/utf8"
+
 import "gohuff/htree"
 
 func main() {
 	var input = loadInput()
+
+	//var asBytes = stringToBytes(input)
+	//fmt.Printf("[%s]\n", string(asBytes))
+	//var asChars = bytesToString(asBytes)
+	//fmt.Printf("[%s]\n", asChars)
+
 	var tree = htree.Create(input)
 	var encoded = htree.Encode(tree, input)
 	var decoded = htree.Decode(tree, encoded)
@@ -30,4 +38,28 @@ func loadInput() string {
 	}
 
 	return input
+}
+
+func stringToBytes(message string) []byte {
+	var totalLength = len(message)
+	var bytes = make([]byte, totalLength)
+	for c := 0; c < totalLength; c++ {
+		theRune, size := utf8.DecodeRuneInString(message)
+		if size > 1 {
+			panic("invalid character")
+		}
+		bytes[c] = byte(theRune)
+		message = message[size:]
+	}
+	return bytes
+}
+
+func bytesToString(bytes []byte) string {
+	var message = ""
+	for len(bytes) > 0 {
+		theRune, _ := utf8.DecodeRune(bytes)
+		message += string(theRune)
+		bytes = bytes[1:]
+	}
+	return message
 }
