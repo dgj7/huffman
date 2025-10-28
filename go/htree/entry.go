c4ed2480b966e0a55057b82e89bf96f527ab0480
dgj7 <garciad48879@gmail.com>
2025-10-27 23:01:33 -0400

    go: table: split entry from table

---
+++ go/htree/entry.go
@@ -0,0 +1,90 @@
+package htree
+
+import "errors"
+
+type Entry struct {
+	key BitSet
+	value byte
+
+	left *Entry
+	right *Entry
+}
+
+func NewLeaf(theKey BitSet, theValue byte) *Entry {
+	return &Entry { key: theKey, value: theValue, left: nil, right: nil }
+}
+
+func InsertEntry(e *Entry, theKey BitSet, theValue byte) bool {
+	if theKey.LessThan(e.key) {
+		//fmt.Printf("\t%s(%d) < %s(%d),", theKey.ToString(), theKey.ToUint64(), e.key.ToString(), e.key.ToUint64())
+		if e.left == nil {
+			//fmt.Printf("new leaf, left\n")
+			e.left = NewLeaf(theKey, theValue)
+			return true
+		} else {
+			//fmt.Printf("calling on e.left\n")
+			return InsertEntry(e.left, theKey, theValue)
+		}
+	} else if theKey.Equals(e.key) {
+		//fmt.Printf("\treturn false, %s already exists\n", theKey.ToString())
+		return false
+	} else {
+		//fmt.Printf("\t%s(%d) > %s(%d),", theKey.ToString(), theKey.ToUint64(), e.key.ToString(), e.key.ToUint64())
+		if e.right == nil {
+			//fmt.Printf("new leaf, right\n")
+			e.right = NewLeaf(theKey, theValue)
+			return true
+		} else {
+			//fmt.Printf("calling on e.right\n")
+			return InsertEntry(e.right, theKey, theValue)
+		}
+	}
+}
+
+func FindEntry(e *Entry, theKey BitSet) (byte, error) {
+	if e == nil {
+		return 0, errors.New("table is empty")
+	}
+
+	if e.key.Equals(theKey) {
+		return e.value, nil
+	}
+
+	if theKey.LessThan(e.key) {
+		if e.left == nil {
+			return 0, errors.New("no byte found for " + theKey.ToString())
+		} else {
+			return FindEntry(e.left, theKey)
+		}
+	} else if theKey.Equals(e.key) {
+		return e.value, nil
+	} else {
+		if e.right == nil {
+			return 0, errors.New("no byte found for " + theKey.ToString())
+		} else {
+			return FindEntry(e.right, theKey)
+		}
+	}
+}
+
+func EntrySize(e *Entry) int {
+	if e == nil {
+		return 0
+	}
+
+	var left = EntrySize(e.left)
+	var right = EntrySize(e.right)
+
+	return 1 + left + right
+}
+
+func EntryHeight(e *Entry) int {
+	if e == nil {
+		return 0
+	}
+
+	var left = EntryHeight(e.left)
+	var right = EntryHeight(e.right)
+
+	return 1 + max(left, right)
+}
--- go/htree/table.go
+++ go/htree/table.go
@@ -1,16 +1,6 @@
 package htree
 
-import "errors"
-
 //import "fmt"
-
-type Entry struct {
-	key BitSet
-	value byte
-
-	left *Entry
-	right *Entry
-}
 
 type Table struct {
 	root *Entry
@@ -24,102 +14,23 @@ func (t *Table) Insert(theKey BitSet, theValue byte) bool {
 	//fmt.Printf("inserting %s(%d)\n", theKey.ToString(), theKey.ToUint64())
 	if t.root == nil {
 		//fmt.Printf("\tnew root: %s\n", theKey.ToString())
-		t.root = newLeaf(theKey, theValue)
+		t.root = NewLeaf(theKey, theValue)
 		return true
 	} else {
-		return insertHelper(t.root, theKey, theValue)
+		return InsertEntry(t.root, theKey, theValue)
 	}
 
 	return false
 }
 
 func (t Table) Find(theKey BitSet) (byte, error) {
-	return findHelper(t.root, theKey)
+	return FindEntry(t.root, theKey)
 }
 
 func (t Table) Size() int {
-	return sizeHelper(t.root)
+	return EntrySize(t.root)
 }
 
 func (t Table) Height() int {
-	return heightHelper(t.root)
-}
-
-func newLeaf(theKey BitSet, theValue byte) *Entry {
-	return &Entry { key: theKey, value: theValue, left: nil, right: nil }
-}
-
-func insertHelper(e *Entry, theKey BitSet, theValue byte) bool {
-	if theKey.LessThan(e.key) {
-		//fmt.Printf("\t%s(%d) < %s(%d),", theKey.ToString(), theKey.ToUint64(), e.key.ToString(), e.key.ToUint64())
-		if e.left == nil {
-			//fmt.Printf("new leaf, left\n")
-			e.left = newLeaf(theKey, theValue)
-			return true
-		} else {
-			//fmt.Printf("calling on e.left\n")
-			return insertHelper(e.left, theKey, theValue)
-		}
-	} else if theKey.Equals(e.key) {
-		//fmt.Printf("\treturn false, %s already exists\n", theKey.ToString())
-		return false
-	} else {
-		//fmt.Printf("\t%s(%d) > %s(%d),", theKey.ToString(), theKey.ToUint64(), e.key.ToString(), e.key.ToUint64())
-		if e.right == nil {
-			//fmt.Printf("new leaf, right\n")
-			e.right = newLeaf(theKey, theValue)
-			return true
-		} else {
-			//fmt.Printf("calling on e.right\n")
-			return insertHelper(e.right, theKey, theValue)
-		}
-	}
-}
-
-func findHelper(e *Entry, theKey BitSet) (byte, error) {
-	if e == nil {
-		return 0, errors.New("table is empty")
-	}
-
-	if e.key.Equals(theKey) {
-		return e.value, nil
-	}
-
-	if theKey.LessThan(e.key) {
-		if e.left == nil {
-			return 0, errors.New("no byte found for " + theKey.ToString())
-		} else {
-			return findHelper(e.left, theKey)
-		}
-	} else if theKey.Equals(e.key) {
-		return e.value, nil
-	} else {
-		if e.right == nil {
-			return 0, errors.New("no byte found for " + theKey.ToString())
-		} else {
-			return findHelper(e.right, theKey)
-		}
-	}
-}
-
-func sizeHelper(e *Entry) int {
-	if e == nil {
-		return 0
-	}
-
-	var left = sizeHelper(e.left)
-	var right = sizeHelper(e.right)
-
-	return 1 + left + right
-}
-
-func heightHelper(e *Entry) int {
-	if e == nil {
-		return 0
-	}
-
-	var left = heightHelper(e.left)
-	var right = heightHelper(e.right)
-
-	return 1 + max(left, right)
+	return EntryHeight(t.root)
 }
