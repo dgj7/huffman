0d4fc07cf0419a45a21a5639c96ba22b5bfb7942
dgj7 <garciad48879@gmail.com>
2025-11-02 19:07:28 -0500

    c: add tests to test file

--- c/test/main.c
+++ c/test/main.c
@@ -8,6 +8,8 @@ int main() {
     struct cut_config_t config = {.print_summary = true};
 
     /* register tests */
+    register_test(empty_string, "empty_string");
+    register_test(single_byte, "single_byte");
     register_test(short_string, "short_string");
     register_test(main_case, "main_case");
 
